322a0fe1b1b932cf081f7dca6b1452a7cc4b3deb
saad786095538 <saadahmed7860999@gmail.com>
2025-03-26 21:18:21 +0500

    growth.py

---
+++ growth.py
@@ -0,0 +1,16 @@
+import streamlit as st
+st.title("Growth Mindset Challenge")
+st.write("""Welcome! Each day, embrace a new challenge to develop a growth mindset.""")
+st.header("Today's Challenge:")
+st.write("***Write down one specific goal you'd like to work on today.***")
+if st.button("✅"):
+    st.write("great job")
+else:
+    st.write("complete")
+
+st.header("Motivational Quote:")
+st.write("""Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful." – Albert Schweitzer""")
+
+
+
+st.write("💡 A growth mindset is the key to success! Keep challenging yourself every day.")
